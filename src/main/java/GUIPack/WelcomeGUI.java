ae7d3c6a6324ea83fae12192af944b358ca424fd
serdar.plg <nyrox.plg@gmail.com>
2023-06-13 16:03:28 +0200

    added

--- src/main/java/GUIPack/WelcomeGUI.java
+++ src/main/java/GUIPack/WelcomeGUI.java
@@ -1,40 +1,53 @@
 package GUIPack;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WelcomeGUI extends JPanel {
     private final JLabel welcomeLabel;
-
     private final JButton continueButton;
+    private final ImageIcon backgroundImage;
+    BufferedImage image;
 
+    protected void paintComponent(Graphics g) {
+        try {
+            image = ImageIO.read(new File("src/main/resources/backround.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        g.drawImage(image, 0, 0, this);
+    }
 
     public WelcomeGUI() {
         setLayout(new BorderLayout());
+        backgroundImage = new ImageIcon("src/main/resources/backround.jpg");
 
         welcomeLabel = new JLabel("Willkommen auf Flugbuchung24");
-        welcomeLabel.setPreferredSize(new Dimension(800,250));
-        welcomeLabel.setSize(new Dimension(800,250));
-        welcomeLabel.setFont(new Font("Bold",Font.BOLD,50));
-        welcomeLabel.setHorizontalTextPosition(JLabel.CENTER);
+        welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        welcomeLabel.setFont(new Font("Bold", Font.BOLD, 50));
 
-        continueButton = new JButton("Weiter");
-        continueButton.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY,10));
+        continueButton = new JButton("Starten");
+        continueButton.setBorder(BorderFactory.createLineBorder(Color.white, 20));
         continueButton.setHorizontalTextPosition(JLabel.CENTER);
         continueButton.addActionListener(new Continue());
-        continueButton.setPreferredSize(new Dimension(200,100));
-        continueButton.setSize(new Dimension(200,100));
+        continueButton.setFont(new Font("Bold", Font.BOLD, 24));
+        continueButton.setPreferredSize(new Dimension(200, 100));
 
-        add(welcomeLabel,BorderLayout.PAGE_START);
-        add(continueButton,BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setOpaque(false);
+        buttonPanel.add(continueButton);
 
-        revalidate();
+        add(welcomeLabel, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.CENTER);
     }
 
     private class Continue implements ActionListener {
-
         @Override
         public void actionPerformed(ActionEvent e) {
             //MyWorker worker = new MyWorker(new LocationPickerGUI());
@@ -42,4 +55,5 @@ public class WelcomeGUI extends JPanel {
             worker.execute();
         }
     }
+
 }
