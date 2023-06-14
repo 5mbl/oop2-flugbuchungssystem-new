0b834d61c2c828c2a250f1ef2ab90652c6175004
serdar.plg <nyrox.plg@gmail.com>
2023-06-14 22:10:32 +0200

    bill view

--- src/main/java/GUIPack/Bill.java
+++ src/main/java/GUIPack/Bill.java
@@ -12,7 +12,10 @@ import java.util.ArrayList;
 public class Bill extends JPanel{
     private final JButton EndSession;
 
+
     private final JPanel InfoPanel;
+    private final JPanel TopPanel;
+
     private String[] InfoArray;
 
     //private final JLabel name;
@@ -22,6 +25,7 @@ public class Bill extends JPanel{
     private final JLabel timeLabel;
     private final JLabel priceLabel;
     private final JLabel departureLable;
+    private final JLabel BillLable;
 
 
 
@@ -36,24 +40,52 @@ public class Bill extends JPanel{
     });
 
 
-
+        TopPanel = new JPanel();
         InfoPanel = new JPanel();
 
+
+
+        // layout
+        // Set layout manager for TopPanel
+        TopPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        TopPanel.setPreferredSize(new Dimension(800, 100)); // Set desired size for TopPanel
+
+        // Set layout manager for InfoPanel
+        InfoPanel.setLayout(new GridLayout(6, 1, 0, 10)); // Adjust grid layout parameters as per your desired spacing
+        InfoPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add some padding to the InfoPanel
+
+
         InfoArray = new String[6];
-        InfoArray[0] = "Name: "+ Airline.get(airlineID).getName();
-        InfoArray[1] = "Destination: "+ Airline.currentDestination.getName();
-        InfoArray[2] = "Date/Time: "+ Airline.get(airlineID).getTimeString();
-        InfoArray[3] = "Seat Number: "+String.valueOf(seatList);
-        InfoArray[4] = "Price: "+String.format("%.2f",((Airline.get(airlineID).getPrice()* Airline.currentDestination.getPaymentFactor())*seatList.size()))+"USD";
-        InfoArray[5] = "Departure: "+ DepartureLocation.getSelectedCity();
+        InfoArray[0] = "Airline Name: "+ Airline.get(airlineID).getName();
+        InfoArray[1] = "Departure: "+ DepartureLocation.getSelectedCity();
+        InfoArray[2] = "Destination: "+ Airline.currentDestination.getName();
+        InfoArray[3] = "Date/Time: "+ Airline.get(airlineID).getTimeString();
+        InfoArray[4] = "Seat Number: "+String.valueOf(seatList);
+        InfoArray[5] = "Price: "+String.format("%.2f",((Airline.get(airlineID).getPrice()* Airline.currentDestination.getPaymentFactor())*seatList.size()))+"USD";
 
         flightLabel = new JLabel(InfoArray[0]);
-        locationLabel = new JLabel(InfoArray[1]);
-        timeLabel = new JLabel(InfoArray[2]);
-        seatLabel = new JLabel(InfoArray[3]);
-        priceLabel = new JLabel(InfoArray[4]);
-        departureLable = new JLabel(InfoArray[5]);
+        departureLable = new JLabel(InfoArray[1]);
+        locationLabel = new JLabel(InfoArray[2]);
+        timeLabel = new JLabel(InfoArray[3]);
+        seatLabel = new JLabel(InfoArray[4]);
+        priceLabel = new JLabel(InfoArray[5]);
 
+
+        Font billFont = new Font("SansSerif", Font.BOLD, 40);
+        Font largerFont = new Font("SansSerif", Font.BOLD, 20); // Adjust the font size (20) as needed
+        // Apply the larger font to the labels
+        flightLabel.setFont(largerFont);
+        locationLabel.setFont(largerFont);
+        timeLabel.setFont(largerFont);
+        seatLabel.setFont(largerFont);
+        priceLabel.setFont(largerFont);
+        departureLable.setFont(largerFont);
+        // Apply billFont to the BillLable
+        BillLable = new JLabel("Thanks for your order - Here is your Bill ");
+        BillLable.setFont(billFont);
+
+
+        TopPanel.add(BillLable);
         InfoPanel.add(flightLabel);
         InfoPanel.add(locationLabel);
         InfoPanel.add(timeLabel);
@@ -61,7 +93,9 @@ public class Bill extends JPanel{
         InfoPanel.add(priceLabel);
         InfoPanel.add(departureLable);
 
-        add(InfoPanel,BorderLayout.CENTER);
+        add(TopPanel, BorderLayout.NORTH);
+        add(InfoPanel, BorderLayout.CENTER);
+
 
         revalidate();
 
